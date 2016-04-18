85f47b95f50b4892451862b4e14dbef3e94adfbc
Danny Libin <omegasol11@gmail.com>
2016-04-18 15:47:11 -0500

    frontend design started

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,94 @@
 import * as React from 'react';
-import * as axios from 'axios';
+import * as _ from 'lodash';
 
 class App extends React.Component<any, any> {
 	
 	constructor() {
 		super();
 		this.state = {
-			response: ''
+			polls: [],
+			selectedPoll: null
 		}
 	}
 	
 	componentWillMount() {
-		axios.get('/test').then(res => this.setState({response: res.data}));
+		for (let i = 0; i < 10; i++) {
+			this.state.polls.push({
+				name: _.random(100, 999)
+			});
+		}
+		this.refreshState();
+	}
+	
+	refreshState() {
+		this.setState({polls: this.state.polls, selectedPoll: this.state.selectedPoll});
+	}
+	
+	pollClick(e, pollClicked) {
+		this.state.selectedPoll = pollClicked;
+		this.refreshState();
 	}
 	
 	render() {
-		return <div>{this.state.response} lalala</div>
+		let details;
+		if (this.state.selectedPoll === null) details = null;
+		else details = (
+			<PollDetails
+				poll={this.state.selectedPoll} />
+		);
+		return (
+			<div id="wrapper">
+				<h1>FCC Voting App</h1>
+				<div id="poll-wrapper">
+					<PollsList
+						polls={this.state.polls}
+						selectedPoll={this.state.selectedPoll}
+						pollClick={(e, pollClicked) => this.pollClick(e, pollClicked)} />
+					{details}
+				</div>
+			</div>
+		);
+	}
+}
+
+class PollsList extends React.Component<any, any> {
+	
+	getListPosition() {
+		let listClass = '';
+		if (this.props.selectedPoll != null) listClass = 'show-details';
+		return listClass;
+	}
+	
+	getPollClass(poll) {
+		let pollClass = 'poll';
+		if (this.props.selectedPoll == poll) pollClass += ' selected';
+		return pollClass;
+	}
+	
+	render() {
+		let polls = this.props.polls.map((poll, index) => {
+			return (
+				<div 
+					className={this.getPollClass(poll)}
+					key={index}
+					onClick={(e) => this.props.pollClick(e, poll)}>
+					{poll.name}
+				</div>
+			)
+		})
+		return (
+			<div id="poll-list" className={this.getListPosition()}>{polls}</div>
+		);
+	}
+}
+
+class PollDetails extends React.Component<any, any> {
+	render() {
+		return (
+			<div className="poll-details">
+				{this.props.poll.name}
+			</div>
+		);
 	}
 }
 
